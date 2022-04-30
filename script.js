d481f5b4bbf054cd2e798e89ef687b3751ae1d06
Stanislav Kozin <stas@staskozin.ru>
2022-04-30 15:58:22 +0300

    Добавил подписи к обязательным полям

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ visit.onchange = function (e) {
   const address = document.querySelector('input[name="address"]')
   if (e.target.checked) {
     address.disabled = false
+    address.required = true
     address.parentElement.classList.remove('label_disabled')
     address.focus()
   } else {
     address.disabled = true
+    address.required = false
     address.parentElement.classList.add('label_disabled')
   }
   console.log(e.target.checked)
